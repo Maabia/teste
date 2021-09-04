8e208bffe0b6b0b8ec4aed211c109401ca8d1c12
Maabia <maabia.ferry16@gmail.com>
2021-09-04 08:02:42 -0300

    interface criada

--- src/app/teste/teste.service.ts
+++ src/app/teste/teste.service.ts
@@ -8,11 +8,14 @@ import { environment } from 'src/environments/environment';
 export class TesteService {
 
   private API = `${environment.API}teste`
-  private headers = { "Content-Type": "application/json" };
-
-
+  // private headers = { "Content-Type": "application/json" };
 
   constructor(private http: HttpClient) { }
+
+  retornaCnpj() {
+    return this.http.get(this.API);
+  }
+
 
 
 }
---
+++ src/app/teste/teste.ts
@@ -0,0 +1,3 @@
+export interface Teste {
+  cnpj: string;
+}
